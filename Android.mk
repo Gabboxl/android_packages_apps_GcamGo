effcfc80ca20c67510a8d6abe97616e07756879b
Gabriele <gabboxl0@gmail.com>
2021-04-19 17:37:48 +0200

    Initial commit

---
+++ Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := GcamGo
+LOCAL_CERTIFICATE := media
+LOCAL_SRC_FILES := com_google_android_apps_cameralite_1_8_332394960_release_7310_minAPI26armeabi.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
